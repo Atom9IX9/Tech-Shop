57b4b81501e1de15e9cc28543450904e01d98a11
Yaroslav <kprogrammer0pr@gmail.com>
2023-06-17 21:22:30 +0300

    Sidebar Header

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -4,19 +4,37 @@ import IconBtn from "../UI/IconBtn";
 import { MouseEventHandler } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../../reducers/appReducer";
+import { AiOutlineUser } from "react-icons/ai";
+import { useTranslation } from "react-i18next";
 
 const SidebarHeader = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { t } = useTranslation()
 
   const closeMenu: MouseEventHandler = (e) => {
-    dispatch(actions.setActiveMenu(false))
+    dispatch(actions.setActiveMenu(false));
   };
 
   return (
     <header className={style.header}>
-      <IconBtn onClick={closeMenu}>
-        <AiOutlineClose color="#fff" size={15} />
-      </IconBtn>
+      <div className={style.close}>
+        <div className={style.logo}></div>
+        <IconBtn onClick={closeMenu}>
+          <AiOutlineClose color="#fff" size={15} />
+        </IconBtn>
+      </div>
+      <div className={style.auth}>
+        <div className={style.authIconWrapper}>
+          <AiOutlineUser color="#fff" size={25} />
+        </div>
+        <div className={style.authContent}>
+          <div className={style.login}>
+            <span className={style.signIn}>{ t("sidebar.Sign in") }</span>
+            <span className={style.signUp}>{ t("sidebar.Sign up") }</span>
+          </div>
+          <p className={style.authText}>{ t("sidebar.auth text") }</p>
+        </div>
+      </div>
     </header>
   );
 };
