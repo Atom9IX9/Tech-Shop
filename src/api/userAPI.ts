fe2bef02f73b2355fe80a465b291ab86832906fd
Yaroslav <kprogrammer0pr@gmail.com>
2023-06-26 23:07:22 +0300

    Update .env LOCATION_API_TOKEN

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse} from "axios";
 const userAPI = {
   getAddress: async (lat: number, lon: number) => {
-    const url = `https://eu1.locationiq.com/v1/reverse?key=${process.env.REACT_APP_ADDRESS_TOKEN}&lat=${lat}&lon=${lon}&format=json`;
+    const url = `https://eu1.locationiq.com/v1/reverse?key=${process.env.REACT_APP_LOCATION_API_TOKEN}&lat=${lat}&lon=${lon}&format=json`;
 
     const response: AxiosResponse<TAddress> = await axios.get(url);
     return response.data.address;
