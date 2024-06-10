32cd95424528ac53e0cf9b4525536d598f93635b
Yaroslav <kprogrammer0pr@gmail.com>
2024-06-10 18:14:02 +0300

    Send BasketProduct Count to Backend

--- src/api/basketAPI.ts
+++ src/api/basketAPI.ts
@@ -1,45 +1,59 @@
-import { $authHost } from "./instance"
+import { $authHost } from "./instance";
 
 const basketAPI = {
   createBasket: async () => {
     try {
       const response = await $authHost.post<TBasket>("api/basket");
-      return response.data
+      return response.data;
     } catch (error: any) {
-      return Promise.reject(error.response.error)
+      return Promise.reject(error.response.error);
     }
   },
   getBasket: async () => {
     try {
       const response = await $authHost.get<TBasket>("api/basket");
-      return response.data
+      return response.data;
     } catch (error: any) {
-      return Promise.reject(error.response.error)
+      return Promise.reject(error.response.error);
     }
   },
   createBasketProduct: async (productId: number, basketId: number) => {
     try {
-      const response = await $authHost.post(`api/basket/product`, { productId, basketId })
-      return response.data
+      const response = await $authHost.post(`api/basket/product`, {
+        productId,
+        basketId,
+      });
+      return response.data;
     } catch (error: any) {
-      return Promise.reject(error.response.data)
+      return Promise.reject(error.response.data);
     }
   },
   getBasketProducts: async (basketId: number) => {
     try {
-      const response = await $authHost.get<TBasketProduct[]>(`api/basket/${basketId}/products`)
-      return response.data
+      const response = await $authHost.get<TBasketProduct[]>(
+        `api/basket/${basketId}/products`
+      );
+      return response.data;
     } catch (error: any) {
-      return Promise.reject(error.response.message)
+      return Promise.reject(error.response.message);
     }
-  }
-}
+  },
+  setBasketProductCount: async (count: number, productId: number) => {
+    try {
+      const response = await $authHost.put<TBasketProduct>(
+        `api/basket/product/${productId}/count`,
+        { count }
+      );
+      return response.data
+    } catch (error) {}
+  },
+};
 
-export default basketAPI
+export default basketAPI;
 export type TBasket = {
-  id: number,
-  userId: number
-}
+  id: number;
+  userId: number;
+};
 export type TBasketProduct = {
   en: string;
   ua: string;
@@ -47,5 +61,6 @@ export type TBasketProduct = {
   price: number;
   sale: number;
   imgs: string;
-  id: number
-}
+  id: number;
+  count: number;
+};
